8399b55eb7c160abee92ac1b54b09d40c0035bbd
Mystalopitech <118692196+Mystalopitech@users.noreply.github.com>
2025-11-04 12:40:25 +0100

    fixed ft_strmapi && ft_putnbr_fd

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: lucy <lucy@42angouleme.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 18:37:35 by lucy              #+#    #+#             */
-/*   Updated: 2025/11/01 18:46:34 by lucy             ###   ########.fr       */
+/*   Updated: 2025/11/04 12:35:33 by eautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t		i;
 	char		*string;
 
-	string = malloc(sizeof (char) * ft_strlen(s));
+	string = malloc(sizeof (char) * ft_strlen(s) + 1);
 	if (!string)
 		return (string);
 	i = 0;
