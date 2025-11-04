47a81f077c97cf8ddc06e167b6f9fdde45dc5759
Mystalopitech <118692196+Mystalopitech@users.noreply.github.com>
2025-11-04 15:58:18 +0100

    added security to putstr_fd

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: eautin <eautin@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 14:39:21 by eautin            #+#    #+#             */
-/*   Updated: 2025/10/25 14:51:01 by eautin           ###   ########.fr       */
+/*   Updated: 2025/11/04 15:57:07 by eautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void	ft_putstr_fd(char *s, int fd)
 	size_t		i;
 
 	i = 0;
+	if (!s)
+	{
+		write (fd, "(null)", 6);
+		return ;
+	}
 	while (s[i])
 	{
 		ft_putchar_fd(s[i], fd);
