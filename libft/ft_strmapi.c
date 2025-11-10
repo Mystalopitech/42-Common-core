d332055eb7e654f678f30a7ddf3732127452f7e0
eautin <mystral.pro@gmail.com>
2025-11-10 13:52:55 +0000

    added security check for strmapi

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: lucy <lucy@42angouleme.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/01 18:37:35 by lucy              #+#    #+#             */
-/*   Updated: 2025/11/04 12:35:33 by eautin           ###   ########.fr       */
+/*   Updated: 2025/11/10 13:52:29 by eautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	size_t		i;
 	char		*string;
 
+	if (!s)
+		return (NULL);
 	string = malloc(sizeof (char) * ft_strlen(s) + 1);
 	if (!string)
 		return (string);
