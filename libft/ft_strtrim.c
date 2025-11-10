23dd7cc056df0f426dc4744c6bc6ede4203eb51d
eautin <mystral.pro@gmail.com>
2025-11-10 13:52:03 +0000

    eddited strtrim for The Norm

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: eautin <eautin@42angouleme.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/25 13:37:43 by eautin            #+#    #+#             */
-/*   Updated: 2025/10/30 17:44:32 by eautin           ###   ########.fr       */
+/*   Updated: 2025/11/10 13:51:44 by eautin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t		source_len;
 	size_t		start;
 	size_t		end;
+	char		*string;
 
 	if (!s1)
 		return (ft_strdup(("")));
@@ -41,5 +42,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 		start++;
 	while (to_trim(s1[end - 1], set))
 		end--;
-	return (ft_substr(s1, start, (end - start)));
+	string = ft_substr(s1, start, (end - start));
+	return (string);
 }
